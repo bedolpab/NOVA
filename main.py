26f0afc3f49389b87660095915b011fbace8016c
pablo <bedol.pab@gmail.com>
2023-03-19 18:03:57 -0500

    sdl2 implement

--- main.py
+++ main.py
@@ -2,21 +2,37 @@
 
 import cv2
 import numpy as np
-import pygame
 import sys
+import sdl2
+import sdl2.ext
 
 W = np.floor_divide(1920, 2)
 H = np.floor_divide(1080, 2)
 
-pygame.init()
-screen = pygame.display.set_mode((W, H))
+sdl2.ext.init()
+
+window = sdl2.ext.Window("Frame", size=(W, H))
+window.show()
+
 
 def get_frame(frame):
+    
+    # Resize
     img = cv2.resize(frame, (W, H), cv2.INTER_AREA)
-    surf = pygame.surfarray.make_surface(img.swapaxes(0,1))
-    screen.blit(surf, (0, 0))
-    pygame.display.update()
-    print(img.shape)
+    
+    # Exit
+    events = sdl2.ext.get_events()
+    for event in events:
+        if event.type == sdl2.SDL_QUIT:
+            exit(0)
+    
+    # Draw
+    windowArray = sdl2.ext.pixels3d(window.get_surface()) 
+    windowArray[:,:,0:3] = img.swapaxes(0,1)    
+    window.refresh()
+
+    # cv2.imshow('frame', img)
+    print(img)
 
 def show_mp4(file):
     cap = cv2.VideoCapture(file)
