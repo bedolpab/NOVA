22047d5e827a325072bf4f8910613c904d476ef8
pablo <bedol.pab@gmail.com>
2023-03-24 10:33:58 -0500

    orb

--- main.py
+++ main.py
@@ -15,11 +15,23 @@ sdl2.ext.init()
 window = sdl2.ext.Window("Frame", size=(W, H))
 window.show()
 
+# INIT ORB
+orb = cv2.ORB_create()
+
 
 def get_frame(frame):
     
     # Resize
     img = cv2.resize(frame, (W, H), cv2.INTER_AREA)
+    
+    # Orb features
+    kp = orb.detect(img, None)
+    kp, des = orb.compute(img, kp)
+    for p in kp:
+        print(f'keypoint: {p.pt}')
+
+    # Draw
+    img_kp = cv2.drawKeypoints(img, kp, None, color=(0, 255, 0), flags=0)
     
     # Exit
     events = sdl2.ext.get_events()
@@ -29,12 +41,13 @@ def get_frame(frame):
     
     # Draw
     windowArray = sdl2.ext.pixels3d(window.get_surface()) 
-    windowArray[:,:,0:3] = img.swapaxes(0,1)    
+    windowArray[:,:,0:3] = img_kp.swapaxes(0,1)    
     window.refresh()
 
     # cv2.imshow('frame', img)
     print(img)
     print(f'size: {img.shape}')
+    input('frame cot: ()')
 
 def show_mp4(file):
     cap = cv2.VideoCapture(file)
