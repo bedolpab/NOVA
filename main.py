feb1e55af10e04efddd9b5242d5786a744e46426
pablo <bedol.pab@gmail.com>
2023-03-31 23:43:31 -0500

    matplotlib GUI

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python3
+
 import cv2
 import numpy as np
 import sys
+from PIL import ImageTk, Image
+import matplotlib.pyplot as plt
 
-(874, 1164, 3)
+# (874, 1164, 3)
 W = 874
 H = 1164
 C = 3
 
+
 def read_frame(frame):
     img = cv2.resize(frame, (W//2, H//4), interpolation=cv2.INTER_AREA)
-    cv2.imshow('Frame', img)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+
+    ax1.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    ax1.set_title("Frame")
+
+    ax2.hist(gray.ravel(), bins=256, range=(0, 256))
+    ax2.set_title("Frame hist")
     print(img.shape)
+    plt.pause(1)
 
 def show_mp4(file):
     cap = cv2.VideoCapture(file)
@@ -24,7 +36,7 @@ def show_mp4(file):
         if ret == True:
             read_frame(frame)
             
-            if cv2.waitKey(5) & 0xFF == ord('q'):
+            if cv2.waitKey(10) & 0xFF == ord('q'):
                 break
         else:
             break
@@ -39,10 +51,10 @@ if __name__ == '__main__':
     # INPUT
     # Get input from video
     file = 'video.hevc'
-
-    show_mp4(file)
-
+    
+    show_mp4(file)    
     # Previous Frame (3, 128, 256)
+
     # Current Frame (3, 128, 256)
     
     # Concat BOTH images --> (6, 128, 256)
